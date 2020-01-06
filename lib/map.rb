435e594843c66fdc5c104458a462ceb401e0007c
pietre.linux <pietre.linux@gmail.com>
2020-01-06 18:27:02 +0100

    Update map.rb

--- lib/map.rb
+++ lib/map.rb
@@ -2,12 +2,12 @@ module Map
   def self.interpret(command)
     responses = []
 
-    if command.match(/^(?:show\s+)?(?:me\s+)?(?:a\s+)?map\s+(?:of\s+)?(.+)$/)
+    if command.match(/^(?:muestra|ensena|abre\s+)?(?:me\s+)?(?:el\s+)?mapa\s+(?:de\s+)?(.+)$/)
       search_term = $1.gsub(' ', '%20')
 
       responses << {
         :command => Command.browser("https://www.google.com/maps/search/#{ search_term }"),
-        :explanation => "Opens a browser with Google Maps searching for '#{ search_term }'."
+        :explanation => "Abre google maps en un navegador con la busqueda'#{ search_term }'."
       }
     end
 
@@ -17,7 +17,7 @@ module Map
   def self.help
     commands = []
     commands << {
-      :category => "Map",
+      :category => "Mapas",
       :description => 'Pull out \033[34mMap\033[0ms from Google',
       :usage => ["show me a map of mountain view"]
     }
